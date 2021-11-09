4cd2f098348ff317a3e07397babe2276bbf1b8e9
Warathaya Rattachai <warathaya.nursewarathaya@mail.kmutt.ac.th>
2021-11-09 16:56:05 +0700

    [Fixed]

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <navBar-layout />
-        <div class="animate-bounce ">
+        <!-- <div class="animate-bounce ">
             <img src="../assets/krathong/kt10.png" >
         </div>
-        
+         -->
        
     </div>
 </template>
