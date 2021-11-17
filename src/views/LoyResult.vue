c02f4d1c6d26097cdcf535caa82d42c3545a80dc
Warathaya Rattachai <warathaya.nursewarathaya@mail.kmutt.ac.th>
2021-11-17 14:26:00 +0700

    [Fixed]Loy result

--- src/views/LoyResult.vue
+++ src/views/LoyResult.vue
@@ -1,19 +1,38 @@
 <template>
   <div>
     <navBar-layout />
-    <content-layout>
-      <p>Wish :{{ this.getWish }}</p>
+    <img src="/src/assets/bg-signIn.png" />
+      <div>
+       
+        <div class="flex justify-center">
+          <img
+            :src="
+              'https://www.loykrathong.tech/api/image/krathongImage/' +
+                this.$route.params.kt_id
+            "
+            class="w-96 p-3  animate-bounce  "
+          />
+       
+        </div>
 
-      <div class="flex justify-center">
-        <img
-          :src="
-            'https://www.loykrathong.tech/api/image/krathongImage/' +
-              this.$route.params.kt_id
-          "
-          class="w-96  animate-bounce"
-        />
+        <div
+          class="rounded-lg bg-lemon text-indigo p-5 w-max flex justify-center"
+        >
+          <div>
+            <!-- <p>{{ this.getWish }}</p>  -->
+            <p>make a wish</p>
+          </div>
+        </div>
       </div>
-    </content-layout>
+      <div class="flex justify-end">
+        <router-link
+          to="/"
+          class=" text-indigo bg-coin rounded-full hover:duration-300 hover:text-silver hover:bg-white p-2 m-10 px-10"
+        >
+          Loy </router-link
+        >
+      </div>
+    
   </div>
 </template>
 <script>
